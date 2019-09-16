64ea5e82e4530b58b9b204babfba3f12b595c281
daiyunchao <312931671@qq.com>
2019-09-16 19:00:48 +0800

    完成了箭头函数和组织reduce的学习

--- 第三节/test.js
+++ 第三节/test.js
@@ -1,23 +1,31 @@
-@type('哺乳类')
-@name('张三')
-class Animal {
-
+//没有compose函数的用法
+function sub(a, b) {
+  return a + b;
 }
 
-let dog=new Animal();
-console.log(dog);
+function len(str) {
+  return str.length;
+}
+
+function addRMB(len) {
+  return '$' + len;
+}
+
+//想组合调用三个函数
+let ret = addRMB(len(sub('abc', 'bcd')));
+console.log(ret);
 
 
-function type(typeName) {
-  console.log(typeName);
-  return function (Constructor) {
-    console.log(Constructor);
+//想要实现:
+let ret_compose = compose(addRMB, len, sub)('abc', 'bcd');
+
+function compose(...fns) {
+  //找到第一个需要执行的函数
+  let lastfn = fns.pop();
+  return function (...args) {
+   return fns.reduceRight((pre, current) => {
+      return current(pre);
+    }, lastfn(...args))
   }
 }
-
-function name(name) {
-  console.log(name);
-  return function (Constructor) {
-    console.log(Constructor);
-  }
-}
+console.log(ret_compose);
