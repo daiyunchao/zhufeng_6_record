8a639a9279050d5af0ca533c0717db418e3b4aad
daiyunchao <312931671@qq.com>
2019-09-17 14:55:37 +0800

    完成了模版引擎的学习

--- 第三节/test.js
+++ 第三节/test.js
@@ -1,31 +1,19 @@
-//没有compose函数的用法
-function sub(a, b) {
-  return a + b;
-}
+let arr = [{ price: 10, count: 5 }, { price: 10, count: 5 }, { price: 10, count: 5 }];
 
-function len(str) {
-  return str.length;
-}
-
-function addRMB(len) {
-  return '$' + len;
-}
-
-//想组合调用三个函数
-let ret = addRMB(len(sub('abc', 'bcd')));
-console.log(ret);
-
-
-//想要实现:
-let ret_compose = compose(addRMB, len, sub)('abc', 'bcd');
-
-function compose(...fns) {
-  //找到第一个需要执行的函数
-  let lastfn = fns.pop();
-  return function (...args) {
-   return fns.reduceRight((pre, current) => {
-      return current(pre);
-    }, lastfn(...args))
+Array.prototype.reduce = function (fn, defaultPre) {
+  let oldArr = JSON.parse(JSON.stringify(arr));
+  let pre = null;
+  if (defaultPre != null && defaultPre != undefined) {
+    pre = defaultPre;
+  } else {
+    pre = arr.shift();
   }
+  arr.forEach((item, index) => {
+    pre = fn(pre, item, index, oldArr);
+  });
+  return pre;
 }
-console.log(ret_compose);
+let total = arr.reduce((pre, current, index, arr) => {
+  return pre + current.price * current.count;
+}, 0)
+console.log(total);
