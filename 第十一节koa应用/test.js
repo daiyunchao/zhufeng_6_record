b58ed3d1e46f53c644f088db1cfee0de6d1aa778
daiyunchao <312931671@qq.com>
2019-12-04 12:12:26 +0800

    完成了koa bodyparser的学习

--- 第十一节koa应用/test.js
+++ 第十一节koa应用/test.js
@@ -1,7 +1,24 @@
 const Koa = require('koa')
 const app = new Koa();
 const fs = require('fs')
-const querystring=require('querystring')
+const querystring = require('querystring')
+const path = require('path')
+const uuid = require('uuid')
+//扩展一个buffer的split方法,用于解析请求体中的buffer
+Buffer.prototype.split = function (sep) {
+  let len = Buffer.from(sep).length; // 分割符的长度
+  let offset = 0;
+  let result = [];
+  let current;
+  // 把找到的位置赋给current  看一下是否为-1
+  while ((current = this.indexOf(sep, offset)) !== -1) {
+    result.push(this.slice(offset, current)); // 把每次的记过push到数组中
+    offset = current + len // 增加查找偏移量
+  }
+  result.push(this.slice(offset)); // 最后一段追加进去
+  return result;
+}
+
 //全部的中间件都需要是async await的
 function bodyParser() {
   return async function (ctx, next) {
@@ -12,7 +29,44 @@ function bodyParser() {
       });
 
       ctx.req.on('end', function () {
-        ctx.request.body =querystring.parse(Buffer.concat(arr).toString());
+        let contentType = ctx.get('Content-Type');
+        console.log("ctx content-type", contentType);
+        if (contentType.includes("form-data")) {
+          //如果是formdata类型,只能解析请求体,因为可能里面的内容不是字符串,而是buffer
+          let data = Buffer.concat(arr);
+          let spe = contentType.split('=')[1];
+          //构建完整的用户分隔符:
+          spe = "--" + spe;
+          // console.log("data===>", data);
+
+          //拆分数据,去掉一头一尾的数据
+          let datas = data.split(spe).slice(1, -1);
+          let requestBody = {};
+          datas.forEach((item) => {
+            let [header, content] = item.split('\r\n\r\n');
+            let isFile = true;
+            header = header.toString();
+            isFile = header.includes('filename');
+            let key = header.match(/name="(.+?)"/)[1];
+            console.log("header==>", key);
+            content = content.split('\r\n')[0].toString();
+            if (!isFile) {
+              //普通字符  
+              requestBody[key] = content;
+            } else {
+              //文件字符
+              //保存文件
+              let filename = uuid.v4()
+              fs.writeFileSync(path.join(__dirname, 'uploads', filename), content, 'utf8');
+              requestBody[key] = filename;
+            }
+          });
+          ctx.request.body = requestBody;
+          reslove();
+        } else {
+          //如果是其他类型,则可直接转换成字符串的形式
+          ctx.request.body = querystring.parse(Buffer.concat(arr).toString());
+        }
         reslove();
       })
     })
